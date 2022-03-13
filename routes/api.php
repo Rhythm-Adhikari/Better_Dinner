66bc237756c15d2ebcdc9574cdc9934bee56d620
Zan Lwine Htoo <zanlwine.htoo110@gmail.com>
2022-03-14 00:59:14 +1100

    login register and logout api

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,11 @@ Route::get('test', function(){
 });
 
 Route::namespace('Api')->group(function(){
+    Route::post('register','AuthController@register');
+    Route::post('login','AuthController@login');
 
     Route::middleware('auth:api')->group(function(){
+        Route::post('logout','AuthController@logout');
     });
 
 });
