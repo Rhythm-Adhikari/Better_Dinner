52f479699c7b8fb698d220b7049b3d729e3f3ab6
Zan Lwine Htoo <zanlwine.htoo110@gmail.com>
2022-03-25 14:19:22 +1100

    v

--- routes/admin_web.php
+++ routes/admin_web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 Route::prefix('admin')->name('admin.')->namespace('Backend')->middleware('auth:admin_user')->group(function(){
     Route::get('/', 'PageController@home')->name('home');
+
+    Route::resource('admin-user','AdminUserController');
 });
