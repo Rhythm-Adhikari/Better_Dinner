0d4c27048a06eae35235ef089b40ad54413aad41
rhythm <rhythm12_adhikari@hotmail.com>
2022-05-10 22:18:03 +1000

    test-login working final

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,52 +1,48 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="about">
-    <H1>About page</H1>
-    <section class="me">
+        <div class="about">
+            <H1>About page</H1>
+            <section class="me">
+            <Section class='head'>
+                <h1 class=''>Welcome to Better Dinner!</h1>
+            </Section>
+                <section class="top">
+                    <section class="introduction-section">
+                        <h1>Who Are We</h1>
+                        <p>We are recent graduates who are desiging a online food ordering/Reservation Website.</p>
+                        <p>We love programming, networking and a bt of a chill time.</p>
+                    </section>
+                    <section class="location-section">
+                        <h1>Where are we Located</h1>
+                        <p>We are a small team based on Wollongong and sydeny </p>
+                    </section>
+                </section>
 
-
-
-
-    <Section class='head'>
-        <h1 class=''>Welcome to Better Dinner!</h1>
-    </Section>
-        <section class="top">
-            <section class="introduction-section">
-                <h1>Who Are We</h1>
-                <p>We are recent graduates who are desiging a online food ordering/Reservation Website.</p>
-                <p>We love programming, networking and a bt of a chill time.</p>
+            <section class="questions-section">
+                <h1>More about Better Dinner</h1>
+                <h2>What is Better Dinner</h2>
+                <p>Better dinne is a atate of the arrt food ordering and delevery system.</p>
+                <h2>how is better dinner better that other known products</h2>
+                <p>better dinner allows resturants a full system that can let them see thair sales , staff rostering, ordering, delivery and reservations.</p>
+                <h2>Where is better dinner implemented</h2>
+                <p>UOW-WOllongong.</p>
+                <h2>Food for thoughts?</h2>
+                <p>Better dinner .</p>
             </section>
-            <section class="location-section">
-                <h1>Where are we Located</h1>
-                <p>We are a small team based on Wollongong and sydeny </p>
             </section>
-        </section>
 
-    <section class="questions-section">
-        <h1>More about Better Dinner</h1>
-        <h2>What is Better Dinner</h2>
-        <p>Better dinne is a atate of the arrt food ordering and delevery system.</p>
-        <h2>how is better dinner better that other known products</h2>
-        <p>better dinner allows resturants a full system that can let them see thair sales , staff rostering, ordering, delivery and reservations.</p>
-        <h2>Where is better dinner implemented</h2>
-        <p>UOW-WOllongong.</p>
-        <h2>Food for thoughts?</h2>
-        <p>Better dinner .</p>
-    </section>
-    </section>
+            <footer class="content-footer">
+                <p3>Say hi to us:</p3>
+                <ul class="social">
+                    <li><a class="css-is-deranged" href="_INSERT_GITHUB_URL_HERE_">GitHub</a></li>
+                    <li><a class="css-is-deranged" href="_INSERT_TWITTER_URL_HERE_">Twitter</a></li>
+                    <li><a class="css-is-deranged" href="_INSERT_GOOGLE+_URL_HERE_">Google+</a></li>
+                </ul>
 
-    <footer class="content-footer">
-        <p3>Say hi to us:</p3>
-        <ul class="social">
-            <li><a class="css-is-deranged" href="_INSERT_GITHUB_URL_HERE_">GitHub</a></li>
-            <li><a class="css-is-deranged" href="_INSERT_TWITTER_URL_HERE_">Twitter</a></li>
-            <li><a class="css-is-deranged" href="_INSERT_GOOGLE+_URL_HERE_">Google+</a></li>
-        </ul>
-
-    </footer>
+            </footer>
 
 
-</div>
+        </div>
 
 @endsection
