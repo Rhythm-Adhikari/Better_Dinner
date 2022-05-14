751eaf9962945cc31fbf98042643d56b0e2cea3e
rhythm <rhythm12_adhikari@hotmail.com>
2022-05-14 17:14:08 +1000

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,30 +1,6 @@
 @extends('layouts.app')
 @section('content')
-{{--<section class="u-align-left u-clearfix u-section-1" id="carousel_4f54">--}}
-{{--      <div class="u-clearfix u-sheet u-sheet-1">--}}
-{{--        <div class="u-clearfix u-expanded-width u-layout-wrap u-layout-wrap-1">--}}
-{{--          <div class="u-layout">--}}
-{{--            <div class="u-layout-row">--}}
-{{--              <div class="u-container-style u-image u-layout-cell u-size-30 u-image-1" data-image-width="1080" data-image-height="1080">--}}
-{{--                <div class="u-container-layout u-container-layout-1"><img class="u-container-style u-image u-layout-cell u-size-30 u-image-1" src="https://live-production.wcms.abc-cdn.net.au/e54c5b975875ff56744dcbf55d399711?impolicy=wcms_crop_resize&cropH=1423&cropW=2529&xPos=385&yPos=488&width=862&height=485">--}}
-{{--              </div>--}}
-{{--              <div class="u-align-left u-container-style u-layout-cell u-shape-rectangle u-size-30 u-layout-cell-2">--}}
-{{--                <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-2">--}}
-{{--                  <h2 class="u-custom-font u-font-montserrat u-text u-text-default u-text-1">About Us</h2>--}}
-{{--                  <div class="u-border-10 u-border-palette-1-base u-line u-line-horizontal u-line-1"></div>--}}
-{{--                  <p class="u-custom-font u-font-montserrat u-text u-text-2">We are recent graduates from University of Wollongong who are desiging a online food ordering/Reservation Website.We love programming, networking and a bit of a chill time.And We are a small team based on Wollongong and Sydney <br>--}}
-{{--                    <br>--}}
-{{--                    <span style="font-weight: 700;">If you wanna find out more about our team, please feel free to call us at anytime</span>--}}
-{{--                  </p>--}}
-{{--                  <a href="https://nicepage.me" class="u-active-none u-btn u-button-style u-hover-none u-none u-text-hover-palette-1-base u-btn-1"><span class="u-icon u-text-palette-1-base"><svg class="u-svg-content" viewBox="0 0 405.333 405.333" x="0px" y="0px" style="width: 1em; height: 1em;"><path d="M373.333,266.88c-25.003,0-49.493-3.904-72.704-11.563c-11.328-3.904-24.192-0.896-31.637,6.699l-46.016,34.752    c-52.8-28.181-86.592-61.952-114.389-114.368l33.813-44.928c8.512-8.512,11.563-20.971,7.915-32.64    C142.592,81.472,138.667,56.96,138.667,32c0-17.643-14.357-32-32-32H32C14.357,0,0,14.357,0,32    c0,205.845,167.488,373.333,373.333,373.333c17.643,0,32-14.357,32-32V298.88C405.333,281.237,390.976,266.88,373.333,266.88z"></path></svg><img></span>&nbsp;+61 0431716888--}}
-{{--                  </a>--}}
-{{--                </div>--}}
-{{--              </div>--}}
-{{--            </div>--}}
-{{--          </div>--}}
-{{--        </div>--}}
-{{--      </div>--}}
-{{--    </section>--}}
+
 <div class="we-are-block">
 
     <div id="about-us-section">
