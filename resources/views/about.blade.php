4a1d4b378df5cd8d046595497471a9275d9dc0cf
Zan Lwine Htoo <zanlwine.htoo110@gmail.com>
2022-05-16 22:11:02 +1000

    css fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,48 +1,50 @@
 @extends('layouts.app')
 @section('content')
+    <div class="we-are-block">
 
-<div class="we-are-block">
+        <div id="about-us-section">
 
-    <div id="about-us-section">
+            <div class="about-us-image">
 
-        <div class="about-us-image">
+                <img src="/img/logo2.jpg" width="808" height="460" alt="betterdinnner">
 
-            <img src="/img/logo2.jpg" width="808" height="460" alt="betterdinnner">
+            </div>
+
+            <div class="about-us-info">
+
+                <h2>A Great way to Order Dinner</h2>
+
+                <p>Better Dinner, located in UOW, makes it easy for people to Order, Dine-In and Take-away their meals.We
+                    believe in our community and want to help everyone enjoy a Better way of dining. The Better dinner team
+                    grades ourselves on smoothest dining experience.</p>
+
+                <a href="#" title="About Us Button">ABOUT US</a>
+
+            </div>
 
         </div>
 
-        <div class="about-us-info">
+        <div id="history-section">
 
-            <h2>A Great way to Order Dinner</h2>
+            <div class="history-image">
 
-            <p>Better Dinner, located in UOW, makes it easy for people to Order, Dine-In and Take-away their meals.We believe in our community and want to help everyone enjoy a Better way of dining. The Better dinner team grades ourselves on smoothest dining experience.</p>
+                <img src="/img/uow.png" width="951" height="471" alt="uow">
 
-            <a href="#" title="About Us Button">ABOUT US</a>
+            </div>
+
+            <div class="history-info">
+
+                <h2>New and Innovative Design</h2>
+
+                <p>We are recent graduates from University of Wollongong who are desiging a online food ordering/Reservation
+                    Website.We love programming, networking and a bit of a chill time.And We are a small team based on
+                    Wollongong and Sydney</p>
+
+                <a href="#" title="History Button">HISTORY</a>
+
+            </div>
 
         </div>
 
     </div>
-
-    <div id="history-section">
-
-        <div class="history-image">
-
-            <img src="/img/uow.png" width="951" height="471" alt="uow">
-
-        </div>
-
-        <div class="history-info">
-
-            <h2>New and Innovative Design</h2>
-
-            <p>We are recent graduates from University of Wollongong who are desiging a online food ordering/Reservation Website.We love programming, networking and a bit of a chill time.And We are a small team based on Wollongong and Sydney</p>
-
-            <a href="#" title="History Button">HISTORY</a>
-
-        </div>
-
-    </div>
-
-</div>
-
 @endsection
